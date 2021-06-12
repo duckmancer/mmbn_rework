fc88b39e933c07218cf4de291507e958b4f66777
duck2 <wunderly.emma@gmail.com>
2021-06-12 14:41:34 -0700

    Added impact animation

--- Battle/Entity/Impact/Impact.gd
+++ Battle/Entity/Impact/Impact.gd
@@ -2,7 +2,13 @@ class_name Impact
 extends Entity
 
 var impact_anim = "hit"
+var rand_cap = 10
+func randomize_offset():
+	var offset = Vector2(rand_range(-rand_cap, rand_cap), rand_range(-rand_cap, rand_cap))
+	sprite.position += offset
+
 func _ready() -> void:
+	randomize_offset()
 	animation_player.play(impact_anim)
 
 func animation_done():
