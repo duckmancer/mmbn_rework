8931e7427dab3dfce6e91c422ea2c69a86f7b912
duck2 <wunderly.emma@gmail.com>
2021-07-04 11:23:31 -0700

    Improved Spritesheet utility.

--- Utilities/SpritesheetManager/SpritesheetManager.gd
+++ Utilities/SpritesheetManager/SpritesheetManager.gd
@@ -9,6 +9,13 @@ enum AnchorType {
 
 const SPRITE_VERTICAL_TOLERANCE = 10
 
+const FRAME_DATA_TEMPLATE = {
+	name = "frame",
+	is_custom_name = false,
+	rect = Rect2(),
+	offset = Vector2(),
+}
+
 export(String, FILE, "*.png") var sheet_path setget set_sheet_path
 export(String, FILE, "*.json") var data_path setget set_data_path
 
@@ -17,15 +24,15 @@ export var frame_index = 0 setget set_frame_index
 export(int) var x_offset = 0 setget set_x_offset
 export(int) var y_offset = 0 setget set_y_offset
 
-export var sprite_name : String setget set_sprite_name
+export var frame_name : String setget set_frame_name
 
 export(AnchorType) var vertical_anchor = AnchorType.END setget set_vertical_anchor
 export(AnchorType) var horizontal_anchor = AnchorType.CENTER setget set_horizontal_anchor
 
 var sprite_offset := Vector2(0, 0) setget set_sprite_offset
 
-var sprite_data : Array = []
-
+var spritesheet_data : Array = []
+var frame_data : Dictionary = {}
 
 # Setgetters
 
@@ -33,7 +40,7 @@ func set_data_path(val : String):
 	if not val.is_abs_path():
 		return
 	data_path = val
-	sprite_data = load_json_data(data_path)
+	spritesheet_data = load_json_data(data_path)
 	set_frame_index(0)
 func set_sheet_path(val : String):
 	if not val.is_abs_path():
@@ -42,31 +49,36 @@ func set_sheet_path(val : String):
 	texture = load(sheet_path)
 	region_enabled = true
 
-func set_frame_index(val : int):
-	if sprite_data.empty():
+func set_frame_index(val : int) -> void:
+	if spritesheet_data.empty():
 		return
 	
-	frame_index = posmod(val, sprite_data.size())
-	load_frame_data()
-	
-	update_sprite()
+	frame_index = posmod(val, spritesheet_data.size())
+	load_frame()
 
-func set_sprite_name(val):
-	sprite_name = val
+func set_frame_name(val : String) -> void:
+	frame_name = val
+	set_frame_data("is_custom_name", not frame_name.empty(), frame_index, false)
+	set_frame_data("name", frame_name, frame_index, false)
+	propagate_frame_names(frame_index)
+	save_json_data(data_path)
+
 
 func set_x_offset(val):
 	x_offset = val
 	set_sprite_offset(Vector2(x_offset, y_offset))
-
 func set_y_offset(val):
 	y_offset = val
 	set_sprite_offset(Vector2(x_offset, y_offset))
 
 func set_sprite_offset(val):
+	_update_sprite_offset(val)
+	update_sprite()
+	set_frame_data("offset", sprite_offset)
+func _update_sprite_offset(val):
 	x_offset = round(val.x)
 	y_offset = round(val.y)
 	sprite_offset = Vector2(x_offset, y_offset)
-	update_sprite()
 
 func set_horizontal_anchor(val):
 	horizontal_anchor = val
@@ -76,69 +88,7 @@ func set_vertical_anchor(val):
 	update_sprite()
 
 
-# Data Processing
-
-func load_frame_data(index = frame_index):
-	var data = sprite_data[index]
-	sprite_name = data.name
-	region_rect = Rect2(data.x, data.y, data.width, data.height)
-	if data.has("sprite_offset"):
-		self.sprite_offset = data.sprite_offset
-	else:
-		self.sprite_offset = Vector2(0, 0)
-
-func save_frame_data(index = frame_index):
-	var data = sprite_data[index]
-	data.name = sprite_name
-	data.x = region_rect.position.x
-	data.y = region_rect.position.y
-	data.width = region_rect.size.x
-	data.height = region_rect.size.y
-	data.sprite_offset = sprite_offset
-	save_json_data(data_path)
-
-
-func unpack_data(data : Array) -> Array:
-	var result = data.duplicate(true)
-	for d in result:
-		if d.has("sprite_offset") and d.sprite_offset is String:
-			d.sprite_offset = str2var(d.sprite_offset)
-	return result
-
-func pack_data(data : Array) -> Array:
-	var result = data.duplicate(true)
-	for d in result:
-		if d.has("sprite_offset") and d.sprite_offset is Vector2:
-			d.sprite_offset = var2str(d.sprite_offset)
-	return result
-
-
-func sort_rects(rect1, rect2) -> bool:
-	if abs(rect1.y - rect2.y) > SPRITE_VERTICAL_TOLERANCE:
-		return rect1.y < rect2.y
-	else:
-		return rect1.x < rect2.x
-
-func load_json_data(path) -> Array:
-	var file = File.new()
-	file.open(path, File.READ)
-	var content = file.get_as_text()
-	file.close()
-	var data = parse_json(content)
-	data.sort_custom(self, "sort_rects")
-	data = unpack_data(data)
-	return data
-
-func save_json_data(path) -> void:
-	var data = pack_data(sprite_data)
-	var file = File.new()
-	file.open(path, File.WRITE)
-	var str_data = JSON.print(data)
-	file.store_string(str_data)
-	file.close()
-
-
-# Display
+# Modifiers
 
 func update_sprite() -> void:
 	offset.x = calc_offset(horizontal_anchor, region_rect.size.x)
@@ -148,7 +98,6 @@ func update_sprite() -> void:
 	else:
 		offset += sprite_offset
 	property_list_changed_notify()
-	save_frame_data()
 
 func calc_offset(type, dimension_size) -> int:
 	var result := 0
@@ -160,7 +109,167 @@ func calc_offset(type, dimension_size) -> int:
 		AnchorType.CENTER:
 			var half_dim = float(dimension_size) / 2
 			if flip_h:
-				result = -ceil(half_dim)
+				result = -ceil(half_dim) as int
 			else:
-				result = -floor(half_dim)
+				result = -floor(half_dim) as int
 	return result
+
+func propagate_frame_names(starting_frame : int) -> void:
+	var base_name := spritesheet_data[starting_frame].name as String
+	var base_index := base_name.to_int()
+	var base_index_str := String(base_index)
+	if not base_index_str in base_name:
+		base_name += base_index_str
+	
+	var index = 1
+	while index + starting_frame < spritesheet_data.size():
+		var cur_index = index + starting_frame
+		index += 1
+		if spritesheet_data[cur_index].is_custom_name:
+			break
+		else:
+			var new_name = base_name.replace(base_index_str, String(index + base_index))
+			set_frame_data("name", new_name, cur_index, false)
+
+
+# Data Processing
+
+func load_frame(index = frame_index) -> void:
+	frame_data = spritesheet_data[index]
+	frame_name = frame_data.name
+	region_rect = frame_data.rect
+	_update_sprite_offset(frame_data.offset)
+	update_sprite()
+
+func set_frame_data(property : String, value, index := frame_index, do_save := true) -> void:
+	assert(property in FRAME_DATA_TEMPLATE.keys())
+	assert(index >= 0 and index < spritesheet_data.size())
+	spritesheet_data[index][property] = value
+	if do_save:
+		save_json_data(data_path)
+
+func pack_data(unpacked_data : Array) -> Array:
+	var data = []
+	for unpacked_frame in unpacked_data:
+		var packed_frame = {}
+		packed_frame.name = unpacked_frame.name
+		packed_frame.is_custom_name = unpacked_frame.is_custom_name
+		packed_frame.rect = var2str(unpacked_frame.rect)
+		packed_frame.offset = var2str(unpacked_frame.offset)
+		data.append(packed_frame)
+	return data
+
+func unpack_data(packed_data : Array) -> Array:
+	var data := []
+	for packed_frame in packed_data:
+		data.append(_unpack_frame(packed_frame))
+	return data
+
+## Data Unpacking
+
+func _unpack_frame(packed_frame : Dictionary) -> Dictionary:
+	var unpacked_frame = {}
+	
+	unpacked_frame.name = _unpack_name(packed_frame)
+	unpacked_frame.is_custom_name = _unpack_is_custom_name(packed_frame)
+	unpacked_frame.rect = _unpack_rect(packed_frame)
+	unpacked_frame.offset = _unpack_offset(packed_frame)
+	
+	return unpacked_frame
+
+func _unpack_name(packed_frame : Dictionary) -> String:
+	var sprite_frame_name = FRAME_DATA_TEMPLATE.name
+	if packed_frame.has("name"):
+		sprite_frame_name = packed_frame.name
+	return sprite_frame_name
+func _unpack_is_custom_name(packed_frame : Dictionary) -> bool:
+	var frame_is_custom_name = FRAME_DATA_TEMPLATE.is_custom_name
+	if packed_frame.has("is_custom_name"):
+		frame_is_custom_name = packed_frame.is_custom_name
+	return frame_is_custom_name
+
+func _unpack_rect(packed_frame : Dictionary) -> String:
+	var frame_rect = FRAME_DATA_TEMPLATE.rect
+	if packed_frame.has("rect"):
+		frame_rect = str2var(packed_frame.rect)
+	else:
+		frame_rect = _unpack_rect_params(packed_frame)
+	return frame_rect
+func _unpack_rect_params(packed_frame : Dictionary) -> Rect2:
+	var frame_rect = FRAME_DATA_TEMPLATE.rect
+	
+	if packed_frame.has("x"):
+		frame_rect.position.x = packed_frame.x
+	if packed_frame.has("y"):
+		frame_rect.position.y = packed_frame.y
+	
+	if packed_frame.has("w"):
+		frame_rect.size.x = packed_frame.w
+	if packed_frame.has("width"):
+		frame_rect.size.x = packed_frame.width
+	
+	if packed_frame.has("h"):
+		frame_rect.size.y = packed_frame.h
+	if packed_frame.has("height"):
+		frame_rect.size.y = packed_frame.height
+	
+	return frame_rect
+
+func _unpack_offset(packed_frame : Dictionary) -> Vector2:
+	var frame_offset = FRAME_DATA_TEMPLATE.offset
+	if packed_frame.has("offset"):
+		frame_offset = str2var(packed_frame.offset)
+	return frame_offset
+
+
+# File I/O
+
+func load_json_data(path : String) -> Array:
+	var data_str = read_file(path)
+	if data_str.empty():
+		data_str = "[]"
+	var raw_data = parse_json(data_str)
+	var unpacked_data = unpack_data(raw_data)
+	unpacked_data.sort_custom(self, "sort_frames")
+	return unpacked_data
+
+func save_json_data(path : String) -> void:
+	var packed_data = pack_data(spritesheet_data)
+	var str_data = JSON.print(packed_data)
+	write_file(path, str_data)
+
+
+func sort_frames(frame1, frame2) -> bool:
+	if frame1.has("rect") and frame2.has("rect"):
+		var pos1 = frame1.rect.position
+		var pos2 = frame2.rect.position
+		if abs(pos1.y - pos2.y) > SPRITE_VERTICAL_TOLERANCE:
+			return pos1.y < pos2.y
+		else:
+			return pos1.x < pos2.x
+	else:
+		return frame1.hash() < frame2.hash()
+
+
+func read_file(path : String) -> String:
+	var contents := ""
+	var file := File.new()
+	var err = file.open(path, File.READ)
+	if err:
+		printerr("Error: ", err)
+		printerr("Could not open file for reading at \"", path, "\"")
+		return ""
+	contents = file.get_as_text()
+	file.close()
+	return contents
+
+func write_file(path : String, contents : String) -> int:
+	var file := File.new()
+	var err = file.open(path, File.WRITE)
+	if err:
+		printerr("Error: ", err)
+		printerr("Could not open file for writing at \"", path, "\"")
+		return err
+	file.store_string(contents)
+	file.close()
+	return 0
