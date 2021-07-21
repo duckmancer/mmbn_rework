8594bfde78df737b84b8b478051688365949361d
duck2 <wunderly.emma@gmail.com>
2021-07-21 09:28:09 -0700

    Added Door types, and editor values for updating on maps.

---
+++ Overworld/MapElements/Objects/Barrier.gd
@@ -0,0 +1,43 @@
+tool
+extends OverworldEntity
+
+const EDITOR_DISPLAY_FRAME := 1
+
+export var flip := false setget set_flip
+export(String, "Green", "Purple", "Skull", "Mystery") var type := "Green" setget set_type
+
+onready var sprite = $AnimatedSprite
+
+
+# SetGet
+
+func set_flip(val : bool) -> void:
+	flip = val
+	if flip:
+		scale.x = -1
+	else:
+		scale.x = 1
+
+func set_type(val : String) -> void:
+	if type != val:
+		type = val
+		if Engine.is_editor_hint():
+			_editor_update_type()
+
+
+# Init
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		sprite.play(type.to_lower())
+
+
+# Editor
+
+func _editor_update_type() -> void:
+	if not is_inside_tree():
+		yield(self, "ready")
+	for child in get_children():
+		if child is AnimatedSprite:
+			child.set_animation(type.to_lower())
+			child.set_frame(EDITOR_DISPLAY_FRAME)
